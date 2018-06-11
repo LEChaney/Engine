6f5cc1e204561d23409619a799f01cdd6b4ea0af
Lance Chaney <lance.e.chaney@gmail.com>
2018-06-11 14:14:40 +1200

    Passed through mouse and keyboard events to ui.

--- ClothSimulation/Screen.cpp
+++ ClothSimulation/Screen.cpp
@@ -5,7 +5,51 @@
 Screen::Screen()
 	: m_uiScreen{ new nanogui::Screen() }
 {
-	m_uiScreen->initialize(Game::getWindowContext(), true);
+	GLFWwindow* window = Game::getWindowContext();
+	m_uiScreen->initialize(window, true);
+	glfwSetWindowUserPointer(window, &m_uiScreen);
+
+	glfwSetCursorPosCallback(window,
+		[](GLFWwindow *, double x, double y) {
+		auto uiScreen = *static_cast<nanogui::ref<nanogui::Screen>*>(glfwGetWindowUserPointer(Game::getWindowContext()));
+		uiScreen->cursorPosCallbackEvent(x, y);
+	});
+
+	glfwSetMouseButtonCallback(window,
+		[](GLFWwindow *, int button, int action, int modifiers) {
+		auto uiScreen = *static_cast<nanogui::ref<nanogui::Screen>*>(glfwGetWindowUserPointer(Game::getWindowContext()));
+		uiScreen->mouseButtonCallbackEvent(button, action, modifiers);
+	});
+
+	glfwSetKeyCallback(window,
+		[](GLFWwindow *, int key, int scancode, int action, int mods) {
+		auto uiScreen = *static_cast<nanogui::ref<nanogui::Screen>*>(glfwGetWindowUserPointer(Game::getWindowContext()));
+		uiScreen->keyCallbackEvent(key, scancode, action, mods);
+	});
+
+	glfwSetCharCallback(window,
+		[](GLFWwindow *, unsigned int codepoint) {
+		auto uiScreen = *static_cast<nanogui::ref<nanogui::Screen>*>(glfwGetWindowUserPointer(Game::getWindowContext()));
+		uiScreen->charCallbackEvent(codepoint);
+	});
+
+	glfwSetDropCallback(window,
+		[](GLFWwindow *, int count, const char **filenames) {
+		auto uiScreen = *static_cast<nanogui::ref<nanogui::Screen>*>(glfwGetWindowUserPointer(Game::getWindowContext()));
+		uiScreen->dropCallbackEvent(count, filenames);
+	});
+
+	glfwSetScrollCallback(window,
+		[](GLFWwindow *, double x, double y) {
+		auto uiScreen = *static_cast<nanogui::ref<nanogui::Screen>*>(glfwGetWindowUserPointer(Game::getWindowContext()));
+		uiScreen->scrollCallbackEvent(x, y);
+	});
+
+	glfwSetFramebufferSizeCallback(window,
+		[](GLFWwindow *, int width, int height) {
+		auto uiScreen = *static_cast<nanogui::ref<nanogui::Screen>*>(glfwGetWindowUserPointer(Game::getWindowContext()));
+		uiScreen->resizeCallbackEvent(width, height);
+	});
 }
 
 void Screen::update()
